d6122269f342f68fe4535a1bdc8af2f7b1273197
Jonnathan <jon_sk322@hotmail.com>
2020-01-28 17:41:07 -0500

    Modulo seguridades

--- sisProvidencias/routes/web.php
+++ sisProvidencias/routes/web.php
@@ -31,3 +31,28 @@ Route::get('/Seguridades/perfilesPorModulo', 'Seguridades\perfilesPorModuloContr
 Route::get('/Seguridades/usuarioAgenciaPerfil', 'Seguridades\usuarioAgenciaPerfilController@index')->name('usuarioAgenciaPerfil');
 Route::get('/Seguridades/horarioAcceso', 'Seguridades\horarioAccesoController@index')->name('horarioAcceso');
 Route::get('/Seguridades/horarioPerfilAgencia', 'Seguridades\horarioPerfilAgenciaController@index')->name('horarioPerfilAgencia');
+
+/* Auto-generated admin routes */
+Route::middleware(['auth:' . config('admin-auth.defaults.guard'), 'admin'])->group(static function () {
+    Route::prefix('admin')->namespace('Admin')->name('admin/')->group(static function() {
+        Route::prefix('admin-users')->name('admin-users/')->group(static function() {
+            Route::get('/',                                             'AdminUsersController@index')->name('index');
+            Route::get('/create',                                       'AdminUsersController@create')->name('create');
+            Route::post('/',                                            'AdminUsersController@store')->name('store');
+            Route::get('/{adminUser}/edit',                             'AdminUsersController@edit')->name('edit');
+            Route::post('/{adminUser}',                                 'AdminUsersController@update')->name('update');
+            Route::delete('/{adminUser}',                               'AdminUsersController@destroy')->name('destroy');
+            Route::get('/{adminUser}/resend-activation',                'AdminUsersController@resendActivationEmail')->name('resendActivationEmail');
+        });
+    });
+});
+
+/* Auto-generated admin routes */
+Route::middleware(['auth:' . config('admin-auth.defaults.guard'), 'admin'])->group(static function () {
+    Route::prefix('admin')->namespace('Admin')->name('admin/')->group(static function() {
+        Route::get('/profile',                                      'ProfileController@editProfile')->name('edit-profile');
+        Route::post('/profile',                                     'ProfileController@updateProfile')->name('update-profile');
+        Route::get('/password',                                     'ProfileController@editPassword')->name('edit-password');
+        Route::post('/password',                                    'ProfileController@updatePassword')->name('update-password');
+    });
+});
